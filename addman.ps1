8aa4f50065f05951c69c8f7bbfe6d6c566d38cd9
hemski635 <53625683+hemski635@users.noreply.github.com>
2019-08-04 12:56:35 +0300

    Update addman.ps1

--- addman.ps1
+++ addman.ps1
@@ -1,214 +1,441 @@
-$addonpath = "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\addons"
-$addonupdaterpath = "$home\Addman" #Change to $userpath for Win10 1903
+$addonpath = "c:/Program Files (x86)/World of Warcraft/_retail_/Interface/addons"
+$addonupdaterpath = "$home/addman" #Change to $userpath for Win10 1903
 
 
 
 
 
-#$ErrorActionPreference = 'silentlycontinue'
-cd $addonupdaterpath
-Import-Module .\testpath.ps1
-$test = test-path .\addonlist.txt
+Write-Host ('
+
+  ___  ______ ______ ___  ___  ___   _   _ 
+ / _ \ |  _  \|  _  \|  \/  | / _ \ | \ | |
+/ /_\ \| | | || | | || .  . |/ /_\ \|  \| |
+|  _  || | | || | | || |\/| ||  _  || . ` |
+| | | || |/ / | |/ / | |  | || | | || |\  |
+\_| |_/|___/  |___/  \_|  |_/\_| |_/\_| \_/
+Simple addons manager for World of Warcraft
+')
+
+
+
+
+
+$ErrorActionPreference = 'stop'
+
+Set-Location $addonupdaterpath
+
+Import-Module ./testpath.ps1
+
+$test = test-path ./addonlist.txt
+
 if ($test -eq $false)
+
     {
+
     New-Item -ItemType file -Name addonlist.txt
-    Add-Content .\addonlist.txt 'addonlist'
+
+    Add-Content ./addonlist.txt 'addonlist'
+
     }
-$test = test-path .\old.txt
+
+$test = test-path ./old.txt
+
 if ($test -eq $false)
+
     {
+
     New-Item -ItemType file -Name old.txt
-    Add-Content .\old.txt 'old'
+
+    Add-Content ./old.txt 'old'
+
     }
 
-function addman{
-Param(
-    [parameter(mandatory=$false)]
-    [Switch]
-    $update,
 
-    [parameter(mandatory=$false)]
-    [Switch]
-    $h,
 
-    [parameter(mandatory=$false)]
-    [String[]]
-    $add,
+function addman
 
-    [parameter(mandatory=$false)]
-    [Switch]
-    $cleanup,
+    {
 
-    [parameter(mandatory=$false)]
-    [String[]]
-    $remove,
+    Param(
 
-    [parameter(mandatory=$false)]
-    [Switch]
-    $list
-    )
+        [parameter(mandatory=$false)]
 
-#Update addons
-if($update){
-    $addons = get-content ".\addonlist.txt"
-    foreach ($addon in $addons){
-        if(($addon -like 'elvui') -or ($addon -like 'tukui'))
+        [Switch]
+
+        $update,
+
+
+
+        [parameter(mandatory=$false)]
+
+        [Switch]
+
+        $h,
+
+
+
+        [parameter(mandatory=$false)]
+
+        [String[]]
+
+        $add,
+
+
+
+        [parameter(mandatory=$false)]
+
+        [Switch]
+
+        $cleanup,
+
+
+
+        [parameter(mandatory=$false)]
+
+        [String[]]
+
+        $remove,
+
+
+
+        [parameter(mandatory=$false)]
+
+        [Switch]
+
+        $list
+
+        )
+
+
+
+    #Update addons
+
+    if($update)
+
+        {
+
+        $addons = get-content "./addonlist.txt"
+
+        foreach ($addon in $addons)
+
             {
-            $elvhref = ((invoke-webrequest "https://www.tukui.org/download.php?ui=$addon").links | Where-Object {$_.href -like "/downloads/$addon-*"}).href
+
+            if(($addon -like 'elvui') -or ($addon -like 'tukui'))
+
+                {
+
+                $elvhref = ((invoke-webrequest "https://www.tukui.org/download.php?ui=$addon").links | Where-Object {$_.href -like "/downloads/$addon-*"}).href
+
+                $elvurl = "https://www.tukui.org$elvhref"
+
+                $elvoutput = "./$addon.zip"
+
+                if((Get-Content ./old.txt).Contains($elvurl))
+
+                    {
+
+                    Write-Host "$addon is already up to date"
+
+                    }
+
+                else
+
+                    {
+
+                    Invoke-WebRequest -Uri $elvurl -OutFile $elvoutput
+
+                    Expand-Archive -Path $elvoutput -DestinationPath $addonpath/ -Force
+
+                    Remove-Item $elvoutput
+
+                    Add-Content ./old.txt $elvurl
+
+                    Write-Host "$addon updated"
+
+                    }
+
+                }
+
+            if(($addon -ne 'addonlist') -and (($addon -notlike 'elvui') -or ($addon -notlike 'tukui')))
+
+                {
+
+                $href = ((invoke-webrequest "https://www.curseforge.com/wow/addons/$addon/download").links | Where-Object {$_.href -like "*file*"}).href
+
+                $url = "https://www.curseforge.com$href"
+
+                $output = "./$addon.zip"
+
+                if((Get-Content ./old.txt).Contains($url))
+
+                    {
+
+                    Write-Host "$addon is up to date"
+
+                    }
+
+                else
+
+                    {
+
+                    Invoke-WebRequest -Uri $url -OutFile $output
+
+                    Expand-Archive -path $output -DestinationPath $addonpath -Force
+
+                    Add-Content ./old.txt $url
+
+                    Remove-Item $output
+
+                    Write-Host "$addon updated"
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+
+    #Install new addon
+
+    if($add)
+
+        {
+
+        if(($add -eq 'elvui') -or ($add -eq 'tukui'))
+
+            {
+
+            $elvhref = ((invoke-webrequest "https://www.tukui.org/download.php?ui=$add").links | Where-Object {$_.href -like "/downloads/$add-*"}).href
+
             $elvurl = "https://www.tukui.org$elvhref"
-            $elvoutput = ".\$addon.zip"
-            if((Get-Content .\old.txt).Contains($elvurl))
+
+            $elvoutput = "./$add.zip"
+
+            if((Get-Content ./old.txt).Contains($elvurl))
+
                 {
-                Write-Host "$addon is already up to date"
+
+                Write-Host "$add is already installed and up to date"
+
                 }
+
             else
+
                 {
+
                 Invoke-WebRequest -Uri $elvurl -OutFile $elvoutput
-                Expand-Archive -Path $elvoutput -DestinationPath $addonpath\ -Force
+
+                Expand-Archive -Path $elvoutput -DestinationPath $addonpath/ -Force
+
                 Remove-Item $elvoutput
-                Add-Content .\old.txt $elvurl
-                Write-Host "$addon updated"
-                }
-            }
-        if(($addon -ne 'addonlist') -and (($addon -notlike 'elvui') -or ($addon -notlike 'tukui')))
-            {
-            $href = ((invoke-webrequest "https://www.curseforge.com/wow/addons/$addon/download").links | Where-Object {$_.href -like "*file*"}).href
-            $url = "https://www.curseforge.com$href"
-            $output = ".\$addon.zip"
-            if((Get-Content .\old.txt).Contains($url))
-                {
-                Write-Host "$addon is up to date"
-                }
-            else
-                {
-                Invoke-WebRequest -Uri $url -OutFile $output
-                Expand-Archive -path $output -DestinationPath $addonpath -Force
-                Add-Content .\old.txt $url
-                Remove-Item $output
-                Write-Host "$addon updated"
-                }
-            }
-        }
-    }
 
-#Install new addon
-if($add)
-    {
-    if(($add -eq 'elvui') -or ($add -eq 'tukui'))
-        {
-        $elvhref = ((invoke-webrequest "https://www.tukui.org/download.php?ui=$add").links | Where-Object {$_.href -like "/downloads/$add-*"}).href
-        $elvurl = "https://www.tukui.org$elvhref"
-        $elvoutput = ".\$add.zip"
-        if((Get-Content .\old.txt).Contains($elvurl))
-            {
-            Write-Host "$add is already installed and up to date"
+                Add-Content ./old.txt $elvurl
+
+                Add-Content ./addonlist.txt $add
+
+                Write-Host "$add installed"
+
+                }
+
             }
+
         else
+
             {
-            Invoke-WebRequest -Uri $elvurl -OutFile $elvoutput
-            Expand-Archive -Path $elvoutput -DestinationPath $addonpath\ -Force
-            Remove-Item $elvoutput
-            Add-Content .\old.txt $elvurl
-            Add-Content .\addonlist.txt $add
-            Write-Host "$add installed"
+
+            $href = ((invoke-webrequest "https://www.curseforge.com/wow/addons/$add/download").links | Where-Object {$_.href -like "*file*"}).href
+
+            $url = "https://www.curseforge.com$href"
+
+            $output = "$addonpath/$add.zip"
+
+            $testeduri = Test-Uri "https://www.curseforge.com/wow/addons/$add/download"
+
+            if($testeduri -eq $true)
+
+                {
+
+                Invoke-WebRequest -Uri $url -OutFile $output
+
+                Expand-Archive -Path $output -DestinationPath "$addonpath/" -Force
+
+                Remove-Item $output
+
+                Add-Content ./addonlist.txt $add
+
+                Add-Content ./old.txt $url
+
+                Write-Host "Installed $add"
+
+                return
+
+                }
+
+            if($testeduri -eq $false)
+
+                {
+
+                Write-Host "$add could not be found. Please check spelling"
+
+                }
+
             }
+
         }
-    else
+
+
+
+    #Remove addon
+
+    if($remove)
+
         {
-        $href = ((invoke-webrequest "https://www.curseforge.com/wow/addons/$add/download").links | Where-Object {$_.href -like "*file*"}).href
-        $url = "https://www.curseforge.com$href"
-        $output = "$addonpath\$add.zip"
-        $testeduri = Test-Uri "https://www.curseforge.com/wow/addons/$add/download"
-        if($testeduri -eq $true)
-            {
-            Invoke-WebRequest -Uri $url -OutFile $output
-            Expand-Archive -Path $output -DestinationPath "$addonpath\" -Force
-            Remove-Item $output
-            Add-Content .\addonlist.txt $add
-            Add-Content .\old.txt $url
-            Write-Host "Installed $add"
-            return
-            }
-        if($testeduri -eq $false)
-            {
-            Write-Host "$add could not be found. Please check spelling"
-            }
+
+        get-content ./addonlist.txt | select-string -pattern $remove -notmatch | out-file ./addonlist.txt
+
+        get-content ./old.txt | select-string -pattern $remove -notmatch | out-file ./old.txt
+
+        Add-Content ./addonlist.txt 'addonlist'
+
+        Add-Content ./old.txt 'old'
+
+        write-host "$remove has been removed from the addons list. To completely remove the addon, run 'addman -cleanup' and 'addman -update'"
+
         }
-    }
 
-#Remove addon
-if($remove)
-    {
-    get-content .\addonlist.txt | select-string -pattern $remove -notmatch | out-file .\addonlist.txt
-    get-content .\old.txt | select-string -pattern $remove -notmatch | out-file .\old.txt
-    Add-Content .\addonlist.txt 'old'
-    Add-Content .\old.txt 'old'
-    write-host "$remove has been removed from the addons list. To completely remove the addon, run 'addman -cleanup' and 'addman -update'"
-    }
 
-#list all addons
-if($list)
-    {
-    cat .\addonlist.txt
-    }
 
-#Clear the addons folder
-if($cleanup)
-    {
-    Write-host "WARNING! This option will delete everything inside your addons directory"
-    $reply = Read-Host -Prompt "Continue?[y/n]"
-    if ( $reply -match "[yY]" )
+    #list all addons
+
+    if($list)
+
         {
-        Remove-Item -Recurse -Force $addonpath\*
-        Write-Host "Addons directory cleaned"
+
+        cat ./addonlist.txt
+
         }
+
+
+
+    #Clear the addons folder
+
+    if($cleanup)
+
+        {
+
+        Write-host "WARNING! This option will delete everything inside your addons directory"
+
+        $reply = Read-Host -Prompt "Continue?[y/n]"
+
+        if ( $reply -match "[yY]" )
+
+            {
+
+            Remove-Item -Recurse -Force $addonpath/*
+
+            Write-Host "Addons directory cleaned"
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+    $manpage = ("
+
+    Addman
+
+
+
+    Name
+
+        addman - World of Warcraft addons manager
+
+
+
+    Synopsis
+
+        addon [option]
+
+
+
+    Description
+
+        Install and update addons from Curseforge
+
+        Options
+
+        -update
+
+            update all addons listed in $home/addman/addonlist.txt
+
+
+
+        -add [addon name]
+
+            install an addon to your pre-set WoW addons directory
+
+
+
+        -remove [addon name]
+
+            Remove an addon from the addonlist
+
+
+
+        -list
+
+            List all addons listed in addonlist
+
+
+
+        -cleanup
+
+            Delete all addons inside your addons folder ($addonpath)
+
+
+
+        -h
+
+            Print this man page
+
+
+
+
+
+    Author
+
+        hemski@hemko.xyz
+
+
+
+
+
+
+
+
+
+
+
+    ")
+
+
+
+    if($h)
+
+        {
+
+        Write-Host $manpage
+
+        }
+
     }
-
-
-
-
-$documentation = ("
-Addman
-
-Name
-    addman - World of Warcraft addons manager
-
-Synopsis
-    addon [option]
-
-Description
-    Install and update addons from Curseforge
-    Options
-    -update
-        update all addons listed in $home\addman\addonlist.txt
-
-    -add [addon name]
-        install an addon to your pre-set WoW addons directory
-
-    -remove [addon name]
-        Remove an addon from the addonlist
-
-    -list
-        List all addons listed in addonlist
-
-    -cleanup
-        Delete all addons inside your addons folder ($addonpath)
-
-    -h
-        Print this man page
-
-
-Author
-    hemski@hemko.xyz
-
-
-
-
-
-")
-
-if($h)
-    {
-    Write-Host $documentation
-    }
-}
